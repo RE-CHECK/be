cd0e89abfaae41d70300b189e4c2e85129dfdb95
imjuyongp <imjuyongp@gmail.com>
2026-03-23 14:44:33 +0900

    :tada:start: initial commit

---
+++ src/main/java/com/be/recheckbe/RecheckBeApplication.java
@@ -0,0 +1,13 @@
+package com.be.recheckbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecheckBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RecheckBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/be/recheckbe/RecheckBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.recheckbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecheckBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
